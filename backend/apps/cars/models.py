2f9180d2b5b57ba8ed8e9fc3895b5e5beae66c61
Elicupra <eliseomog1989@gmail.com>
2026-01-26 12:40:16 +0100

    Add django_filters import and clean up models.py for Car class

--- backend/apps/cars/models.py
+++ backend/apps/cars/models.py
@@ -1,19 +1,22 @@
-# from django.db import models
+from django.db import models
 
 # # Create your models here.
 
-# fuel = [
-#     ('petrol', "PETROL" ), ('diesel' , 'DIESEL') , ('ev' , 'EV')
-# ]
+fuel = [
+     ('petrol', "PETROL" ), ('diesel' , 'DIESEL') , ('ev' , 'EV')
+ ]
+no_of_seats = range(2, 11)
+transmission_types = [
+    ('manual', 'MANUAL'),
+    ('automatic', 'AUTOMATIC')
+]
 
-# no_of_seats = [
-#     ()
-# ]
-# class Carz(models.Model):
-#     car_name = models.CharField(max_length=100)
-#     company = models.CharField(max_length=100)
-#     color = models.CharField(max_length=100)
-#     fuel_type = models.CharField(max_length=100 , choices=fuel)
-#     seat_capacity = models.CharField(max_length=10, choices= no_of_seats)
-#     transmission_type = models.CharField(max_length=10, choices= trasmission)
-#     total_km_driven = 
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    fuel_type = models.CharField(max_length=100 , choices=fuel)
+    seat_capacity = models.CharField(max_length=10, choices= no_of_seats)
+    transmission_type = models.CharField(max_length=10, choices= transmission_types)
+    total_km_driven = models.IntegerField()
