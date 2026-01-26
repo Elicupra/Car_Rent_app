eacc5c515f8c5aa4ca66e48aeab442300419838a
Elicupra <eliseomog1989@gmail.com>
2026-01-26 13:01:12 +0100

    Update import statements in app.py for better organization

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,7 @@
+"""Main application file for backend."""
+
+
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
-from backend.apps.cars.models import Car
+from backend.apps.cars import admin,apps,models
