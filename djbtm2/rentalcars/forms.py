919a4bf6d75ae62cf84e8cca68324f10e7ba880a
Elicupra <eliseomog1989@gmail.com>
2026-02-02 15:41:14 +0100

    Add CarzForm with validation and  toggle availability action in forms.py

--- djbtm2/rentalcars/forms.py
+++ djbtm2/rentalcars/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from rentalcars.models import Carz
 
 class RentDetailsForm(forms.Form):
     total_no_days = forms.IntegerField(
@@ -29,3 +29,27 @@ class FinalRentDetailsForm(forms.Form):
         min_value=1,
         widget=forms.NumberInput(attrs={'class': 'form-control'})
     )
+
+class CarzForm(forms.ModelForm):
+    class Meta:
+        model = Carz
+        fields = '__all__'
+    
+    def clean_price_per_day(self):
+        price = self.cleaned_data.get('price_per_day')
+        if price and price <= 0:
+            raise forms.ValidationError("The price per day must be positive")
+        return price
+    
+    def clean_rating(self):
+        rating = self.cleaned_data.get('rating')
+        if rating and not (0 <= rating <= 5.0):
+            raise forms.ValidationError("The rating must be between 0 and 5.0")
+        return rating
+    
+    def toggle_availability(self, request, queryset):
+        count = queryset.update(is_available=~F('is_available'))
+        self.message_user(request, f"{count} autos actualizados")
+    toggle_availability.short_description = "Toggle availability"
+
+    actions = [toggle_availability]
