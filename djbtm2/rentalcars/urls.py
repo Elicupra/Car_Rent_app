b2ffdb6c40b6841446ba0a5fb9309328d3ccc9b8
Elicupra <eliseomog1989@gmail.com>
2026-02-02 15:00:14 +0100

    Add API endpoint for Carz using Django REST Framework

--- djbtm2/rentalcars/urls.py
+++ djbtm2/rentalcars/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
+
+#New imports for ViewSet
+
+from rest_framework.routers import DefaultRouter
+from rentalcars.views import CarzViewSet
+
+router = DefaultRouter()
+router.register(r'carz', CarzViewSet)
 
 urlpatterns = [
     path("rentalcars/", views.rentalcars_view, name="rentalcars"),
@@ -7,4 +15,6 @@ urlpatterns = [
     path   ("rental-car/<int:id>/", views.rentalcar_details, name="rentalcar_details"),
     path   ("rent-details/<int:id>/", views.rent_details, name="rent_details"),
     path   ("final-rent-details/<int:id>/", views.final_rent_details, name="final_rent_details"),
+        # API REST
+    path('api/', include(router.urls)),
 ]
